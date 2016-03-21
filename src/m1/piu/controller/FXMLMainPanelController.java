47ecf6c51df4338076922ee474e4e7b0c656d91b
adri84 <adrian.salas84@gmail.com>
2016-03-21 17:20:30 +0100

    -init

---
+++ src/m1/piu/controller/FXMLMainPanelController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m1.piu.controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+/**
+ * FXML Controller class
+ *
+ * @author uapv1302368
+ */
+
+
+public class FXMLMainPanelController implements Initializable {
+
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+               
+        
+    }    
+    
+}
